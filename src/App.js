773eda8ebe31613e9cf9c1ba5cb778488888c9cb
PhamChiXuan1 <99774231+PhamChiXuan1@users.noreply.github.com>
2024-04-17 12:47:13 +0700

    add image

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <h2>20036191-Nguyễn Quốc Khôi</h2>
       <h2>20030131-Phạm Chí Xuân</h2>
       <h2>Xin chào mọi người</h2>
+      <Image src={"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwebera.blog%2Fhow-to-create-your-own-custom-action-for-using-in-github-actions-workflows-41744264fc32&psig=AOvVaw04o69sJ8ey2EtaGseYXlHl&ust=1713419173134000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCNC2rPvFyIUDFQAAAAAdAAAAABAE"}/>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
