dcdfc0508579c7252c254ed458d98e485bd71333
PhamChiXuan1 <99774231+PhamChiXuan1@users.noreply.github.com>
2024-04-17 13:03:22 +0700

    change image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import gitAc from './img/githubAction.jpg';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <h2>20036191 - Nguyễn Quốc Khôi</h2>
       <h2>20030131 - Phạm Chí Xuân</h2>
       <h2>Xin chào mọi người</h2>
-      <Image src={"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwebera.blog%2Fhow-to-create-your-own-custom-action-for-using-in-github-actions-workflows-41744264fc32&psig=AOvVaw04o69sJ8ey2EtaGseYXlHl&ust=1713419173134000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCNC2rPvFyIUDFQAAAAAdAAAAABAE"}/>
+      <img src={gitAc} className="GitAc-img" alt="image"/>
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
